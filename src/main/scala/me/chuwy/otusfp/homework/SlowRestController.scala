a7774841b3c14d9143527451c5135f8c986d810a
ReDestroyDeR <daniil.schroder@gmail.com>
2022-10-23 22:20:25 +0300

    Clear slow

--- src/main/scala/me/chuwy/otusfp/homework/SlowRestController.scala
+++ src/main/scala/me/chuwy/otusfp/homework/SlowRestController.scala
@@ -4,10 +4,8 @@ import cats.effect.IO
 import cats.effect.implicits._
 import cats.effect.std.Random
 import cats.implicits._
-import io.circe.generic.auto.exportEncoder
 import org.http4s.HttpRoutes
 import org.http4s.Uri.Path.Root
-import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 
@@ -16,11 +14,10 @@ import scala.concurrent.duration.DurationInt
 object SlowRestController {
 
   def slowService: HttpRoutes[IO] = HttpRoutes.of{
-    case r @ GET -> Root / "slow" / chunk / total / time =>
-      Ok(fs2.Stream.awakeEvery[IO](time.toInt.seconds)
-        .evalMap(m => Random.scalaUtilRandom[IO].flatMap(_.nextBytes(chunk.toInt)))
-        .flatMap(fs2.Stream(_:_*))
-        .take(total.toInt)
-        .evalTap(IO.println(_)))
+    case r @ GET -> Root / "slow" / IntVar(chunk) / IntVar(total) / IntVar(time) =>
+      Ok(fs2.Stream.awakeEvery[IO](time.seconds)
+        .evalMap(m => Random.scalaUtilRandom[IO].flatMap(_.nextBytes(chunk)))
+        .flatMap(b => fs2.Stream(b:_*))
+        .take(total))
   }
 }
