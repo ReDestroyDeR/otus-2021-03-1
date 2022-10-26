ac5f6631877122ca498e308e4a751abe99652f96
ReDestroyDeR <daniil.schroder@gmail.com>
2022-10-26 04:00:45 +0300

    Refactor Slow

--- src/main/scala/me/chuwy/otusfp/homework/SlowRestController.scala
+++ src/main/scala/me/chuwy/otusfp/homework/SlowRestController.scala
@@ -10,14 +10,18 @@ import org.http4s.dsl.io._
 import org.http4s.implicits._
 
 import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 object SlowRestController {
 
   def slowService: HttpRoutes[IO] = HttpRoutes.of{
     case r @ GET -> Root / "slow" / IntVar(chunk) / IntVar(total) / IntVar(time) =>
-      Ok(fs2.Stream.awakeEvery[IO](time.seconds)
-        .evalMap(m => Random.scalaUtilRandom[IO].flatMap(_.nextBytes(chunk)))
-        .flatMap(b => fs2.Stream(b:_*))
-        .take(total))
+      Ok(
+        fs2.Stream.evalSeq(Random.scalaUtilRandom[IO]
+                                 .flatMap(_.nextBytes(total)
+                                           .map(_.toSeq)))
+          .chunkLimit(chunk)
+          .metered(time seconds)
+      )
   }
 }
